fd3cf9d01e12777e5f08d1d4d44faaf7b4e4b96b
vishalkumar <rajvishal71760@gmail.com>
2026-06-08 10:58:36 +0530

    code modified

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-pink-300 p-4 rounded-xl bg-green-300 line-through' >Tailwind</h1>
+    </>
+  )
+}
+
+export default App
