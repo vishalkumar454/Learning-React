66991eecc46d2f5cdb0edfda78f7296bdeaacab5
vishalkumar <rajvishal71760@gmail.com>
2026-06-11 13:31:16 +0530

    code optimized

---
+++ 05-password-Generator/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { useState , useCallback } from 'react'
+import './App.css'
+
+function App() {
+
+  const [length , setLength] = useState(8);
+  const [numberAllowed , setNumberAllowed] = useState(false);
+  const [charAllowed , setCharAllowed] = useState(false);
+  const [password , setPassword] = useState("");
+
+  const passwordGenerator = () => {
+
+  }
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>
+        Password Generator
+      </h1>
+    </>
+  )
+}
+
+export default App
